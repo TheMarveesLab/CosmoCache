c725270dee5aefab4b1a012e16af7edb6b559827
TheMarveesLab <marvee@themarveeslab.com>
2024-08-13 19:02:32 +0100

    fix: race condition

--- cosmocache.go
+++ cosmocache.go
@@ -1,12 +1,16 @@
 package main
 
+import "sync"
+
 type CosmoCache interface {
 	Set(key string, val string) error
 	Get(key string) (string, bool)
 	Del(key string)
+	Flush()
 }
 
 type cache struct {
+	mu   sync.RWMutex
 	data map[string]string
 }
 
@@ -17,15 +21,25 @@ func NewCache() *cache {
 }
 
 func (c *cache) Set(key string, val string) error {
+	c.mu.Lock()
 	c.data[key] = val
+	c.mu.Unlock()
 	return nil
 }
 
 func (c *cache) Get(key string) (string, bool) {
+	c.mu.RLock()
 	val, ok := c.data[key]
+	c.mu.RUnlock()
 	return val, ok
 }
 
 func (c *cache) Del(key string) {
+	c.mu.Lock()
 	delete(c.data, key)
+	c.mu.Unlock()
+}
+
+func (c *cache) Flush() {
+	c.data = make(map[string]string)
 }
--- cosmocache_test.go
+++ cosmocache_test.go
@@ -1,38 +1,111 @@
 package main
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
-func TestCache(t *testing.T) {
+func TestCacheGet(t *testing.T) {
+	cc := NewCache()
 
-	c := NewCache()
-
-	if val1, ok := c.Get("key1"); ok {
+	if val1, ok := cc.Get("key1"); ok {
 		t.Errorf("should not find any value: %v", val1)
 	}
-	if val2, ok := c.Get("key2"); ok {
+	if val2, ok := cc.Get("key2"); ok {
 		t.Errorf("should not find any value: %v", val2)
 	}
 
-	if err := c.Set("key1", "val 1"); err != nil {
-		t.Errorf("should not return error: %v", err)
+	cc.data["key1"] = "value for key1"
+	cc.data["key2"] = "value for key2"
+
+	val1, ok := cc.Get("key1")
+	if !ok {
+		t.Error("could not find value for key1")
 	}
-	if err := c.Set("key2", "val 2"); err != nil {
-		t.Errorf("should not return error: %v", err)
+	if val1 != "value for key1" {
+		t.Error("should return `value for key1`")
+	}
+	val2, ok := cc.Get("key2")
+	if !ok {
+		t.Error("could not find value for key2")
+	}
+	if val2 != "value for key2" {
+		t.Error("should return `value for key2`")
+	}
+}
+
+func TestCacheSet(t *testing.T) {
+	cc := NewCache()
+
+	cc.Set("key1", "value for key1")
+
+	val, ok := cc.data["key1"]
+	if !ok {
+		t.Error("should get key `key1`")
 	}
 
-	val1, ok := c.Get("key1")
-	if !ok {
-		t.Errorf("could not find value for key1")
+	if val != "value for key1" {
+		t.Error("should return `value for key1`")
 	}
-	if val1 != "val 1" {
-		t.Errorf("should return `val 1`")
-	}
-	val2, ok := c.Get("key2")
-	if !ok {
-		t.Errorf("could not find value for key2")
-	}
-	if val2 != "val 2" {
-		t.Errorf("should return `val 2`")
+}
+
+func TestCacheDel(t *testing.T) {
+	cc := NewCache()
+	cc.data["key1"] = "val for key1"
+	cc.data["key2"] = "val for key2"
+	cc.data["key3"] = "val for key3"
+
+	cc.Del("key2")
+
+	if _, ok := cc.data["key2"]; ok {
+		t.Error("should not find key2")
 	}
 
+	if len(cc.data) != 2 {
+		t.Error("should have 2 items")
+	}
+}
+
+func TestCacheFlush(t *testing.T) {
+	cc := NewCache()
+	cc.data["key1"] = "val for key1"
+	cc.data["key2"] = "val for key2"
+	cc.data["key3"] = "val for key3"
+
+	cc.Flush()
+
+	if len(cc.data) != 0 {
+		t.Error("should have no items")
+	}
+}
+
+func TestCacheRaceConditrion(t *testing.T) {
+	cc := NewCache()
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			cc.Set("key1", "value for key1")
+		}()
+	}
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			_, _ = cc.Get("key1")
+		}()
+	}
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			cc.Del("key1")
+		}()
+	}
+	wg.Wait()
 }
